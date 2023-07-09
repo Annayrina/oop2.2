111eab646694a4105786507d420aa145f0b45a1f
annayrina <annayrina1998@mail.ru>
2023-07-09 20:01:57 +0700

    done

---
+++ src/Transport/Transport.java
@@ -0,0 +1,24 @@
+package Transport;
+public abstract class Transport {
+    private final int wheelsCount;
+    private final String modelName;
+
+    public Transport(String modelName, int wheelesCount) {
+        this.wheelsCount = wheelesCount;
+        this.modelName = modelName;
+    }
+        public String getModelName() {
+            return modelName;
+        }
+
+        private void updateTyre() {
+            System.out.println("Меняем покрышку");
+        }
+
+        public void service() {
+            System.out.println("Обслуживаем " + this.modelName);
+            for (int i = 0; i < this.wheelsCount; i++) {
+                this.updateTyre();
+            }
+        }
+    }
